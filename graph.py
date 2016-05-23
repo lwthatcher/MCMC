a1889a1bacd83ffcd0d867f54338ab4964049a54
Lawrence Thatcher <lwthatcher@msn.com>
2016-05-23 15:02:49 -0600

    Graph constructor now also stores inverse connections

--- graph.py
+++ graph.py
@@ -3,8 +3,14 @@ class Graph:
     def __init__(self, connections, nodes):
         self.connections = connections
         self.nodes = nodes
+
         for node in self.nodes:
             node._graph = self
+
+        self.inv_connections = {}
+        for node, children in self.connections.items():
+            for child in children:
+                self.inv_connections[child] = self.inv_connections.setdefault(child, []) + [node]
 
 
 class Node:
