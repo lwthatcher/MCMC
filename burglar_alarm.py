5a7d7ee3a1c9183b957d0fefe8aac6e6ffbcf67d
Lawrence Thatcher <lwthatcher@msn.com>
2016-05-24 12:36:57 -0600

    prints true_mean and false_mean for burglar example

--- burglar_alarm.py
+++ burglar_alarm.py
@@ -5,8 +5,8 @@ from graph import Node
 
 
 def alarm():
-    nodes = [Node('B', {(): 0.001}, val=1.),
-             Node('E', {(): 0.002}, val=1.),
+    nodes = [Node('B', {(): 0.001}, val=0.),
+             Node('E', {(): 0.002}, val=0.),
              Node('A', {(1, 1): 0.95, (1, 0): 0.94, (0, 1): 0.29, (0, 0): 0.001}, val=0.),
              Node('J', {(1,): 0.9, (0,): 0.05}, observed=True),
              Node('M', {(1,): 0.7, (0,): 0.01}, observed=True)]
--- mcmc.py
+++ mcmc.py
@@ -1,4 +1,5 @@
 
+import numpy as np
 from burglar_alarm import alarm
 
 
@@ -8,10 +9,10 @@ class MCMC:
         self.graph = alarm()
 
     def iteration(self):
-        result = []
+        result = {}
         for node in self.graph.hidden_nodes:
             xi = node.sample()
-            result.append(xi)
+            result[node.name] = xi
         return result
 
     def gibbs(self, burn, n_samples):
@@ -21,12 +22,25 @@ class MCMC:
         # take n samples
         samples = [self.iteration() for i in range(n_samples)]
 
-        print(samples)
+        return samples
 
+
+def sample_dim(samples, dim):
+    d = [s[dim] for s in samples]
+    true_mean = np.mean(d)
+    false_mean = 1 - true_mean
+    return true_mean, false_mean
 
 def main():
     mcmc = MCMC()
-    mcmc.gibbs(1, 1000)
+    samples = mcmc.gibbs(10000, 10000)
+    mean, f_mean = sample_dim(samples, 'B')
+    print("P(Burglary | JohnCalls=true, MaryCalls=true) = <", mean, ", ", f_mean, ">")
+    mean, f_mean = sample_dim(samples, 'A')
+    print("P(Alarm | JohnCalls=true, MaryCalls=true) = <", mean, ", ", f_mean, ">")
+    mean, f_mean = sample_dim(samples, 'E')
+    print("P(Earthquake | JohnCalls=true, MaryCalls=true) = <", mean, ", ", f_mean, ">")
+
 
 if __name__ == '__main__':
     main()
