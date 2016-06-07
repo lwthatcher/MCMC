4928fc47b59d8b2c1423c9c328d85f0ad115b768
Lawrence Thatcher <lwthatcher@msn.com>
2016-06-07 14:44:24 -0600

    created missing data generator

---
+++ missing_data_generator.py
@@ -0,0 +1,20 @@
+import json
+import random
+
+model = '01'
+p = 0.10
+
+file_name = 'alarm-gen-' + model + '.json'
+with open(file_name, 'r') as f:
+    data = json.load(f)
+
+for row in data:
+    for key in row:
+        u = random.random()
+        if u > 1-p:
+            row[key] = 'x'
+
+P = p*100
+out_file_name = 'alarm-gen-' + model + '_' + str(P) + '.json'
+with open(out_file_name, 'w') as of:
+    json.dump(data, of)
